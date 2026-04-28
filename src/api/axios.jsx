1fecfbd2e4ea09893af74c803ce2914d1e8063ab
Mohammed Shaheem <mohammedshaheemtk2@gmail.com>
2026-04-28 19:41:08 +0530

    fix:trainer dashboard

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -5,7 +5,7 @@ import.meta.env.VITE_API_URL
 
     
 const api = axios.create({
-    baseURL: meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
